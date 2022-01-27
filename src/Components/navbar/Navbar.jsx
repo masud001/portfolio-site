397d93b0547598d8b8b9d039d8914f39dd33df9e
masud001 <madhnagar@gmail.com>
2022-01-28 00:55:46 +0600

    design start Nav-bar

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 const Navbar = () => {
 	return (
 		<div>
-			<h1 className='gradient__text'>
-				Navbar Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, eaque.
-			</h1>
+			<h1 className='gradient__text'>Navbar</h1>
 		</div>
 	);
 };
