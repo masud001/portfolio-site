30aab71b2c315457f3a814da7c03a4dda067e163
masud001 <madhnagar@gmail.com>
2022-01-28 02:32:21 +0600

    navbar component completed

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,9 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import './Navbar.css';
+import logo from '../../assets/logo.svg';
+
+const Menu = () => (
+	<React.Fragment>
+		<p>
+			<a href='#home'>home</a>
+		</p>
+		<p>
+			<a href='#whatmdr'>What is MDR?</a>
+		</p>
+		<p>
+			<a href='#possibility'>Open AI</a>
+		</p>
+		<p>
+			<a href='#features'>Case Studies</a>
+		</p>
+		<p>
+			<a href='#blog'>Library</a>
+		</p>
+	</React.Fragment>
+);
 
 const Navbar = () => {
+	const [toggleMenu, setToggleMenu] = useState(false);
 	return (
-		<div>
-			<h1 className='gradient__text'>Navbar</h1>
+		<div className='mdr__navbar'>
+			{/* links  */}
+			<div className='mdr__navbar-links'>
+				{/* logo  */}
+				<div className='mdr__navbar-links_logo'>
+					<img src={logo} alt={logo} className='logo' />
+				</div>
+				<div className='mdr__navbar-links_container'>
+					<Menu />
+				</div>
+			</div>
+			{/* sign up/in  */}
+			<div className='mdr__navbar-sign'>
+				<p>sign in</p>
+				<button>sign up</button>
+			</div>
+			{/* menu for mobile  */}
+			<div className='mdr__navbar-menu'>
+				{toggleMenu ? (
+					<RiCloseLine
+						color='#fff'
+						size={27}
+						onClick={() => {
+							setToggleMenu(false);
+						}}
+					/>
+				) : (
+					<RiMenu3Line
+						color='#fff'
+						size={27}
+						onClick={() => {
+							setToggleMenu(true);
+						}}
+					/>
+				)}
+				{toggleMenu && (
+					<div className='mdr__navbar-menu_container scale-up-center '>
+						<div className='mdr__navbar-menu_container-links'>
+							<Menu />
+							<div className='mdr__navbar-menu_container-links-sign'>
+								<p>sign in</p>
+								<button>sign up</button>
+							</div>
+						</div>
+					</div>
+				)}
+			</div>
 		</div>
 	);
 };
